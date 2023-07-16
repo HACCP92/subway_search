51b6f2611ee14cdabef443679aa8be99795e3e31
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-16 23:50:14 +0900

    [지하철 실시간 정보 앱] router 오류 수정

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:subway_search/data/repository/swopen_station_repository_impl.dart';
+import 'package:subway_search/domain/use_case/get_top_most_arrive_info_use_case.dart';
+import 'package:subway_search/presentation/main/main_screen.dart';
+import 'package:subway_search/presentation/main/main_view_model.dart';
 
 final GoRouter router = GoRouter(
   routes: <RouteBase>[
@@ -8,7 +12,9 @@ final GoRouter router = GoRouter(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
         return ChangeNotifierProvider(
-          create: (_) => MainViewModel(),
+          create: (_) => MainViewModel(GetTopMostArriveInfoUseCase(
+            SwopenRepositoryImpl(),
+          )),
           child: const MainScreen(),
         );
       },
